8545f867ba3ea6f3302374aa92ed9edf0c265d8f
Maria alejandra g <marialeja32506@hotmail.com>
2022-08-04 16:21:53 -0500

    pokemon app

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import { Navbar } from "../ui";
+
+interface Props {
+  children: React.ReactNode;
+  title?: string;
+}
+
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
+export const Layout: React.FC<Props> = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title || "PokemonApp"}</title>
+        <meta name="author" content="Maria alejandra G" />
+        <meta
+          name="description"
+          content={`Informacion sobre el pokemon ${title}`}
+        />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Informacion sobre  ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la pagina sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/imgs/banner.png`} />
+      </Head>
+
+      <Navbar />
+
+      <main
+        style={{
+          padding: "0px 20px",
+        }}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
